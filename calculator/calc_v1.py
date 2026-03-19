4fad3be010c6aa98ba5c7688e18fe6037f2cd401
xdking9999-cpu <xdking9999@gmail.com>
2026-03-19 08:26:04 +0530

    Add calculator versions 1, 2, and 3

--- calculator/calc_v1.py
+++ calculator/calc_v1.py
@@ -1,23 +1,73 @@
-# Version 1 Calculator
+import math
 
-def add(x, y):
-    return x + y
 
-def subtract(x, y):
-    return x - y
+print("  Welcome To Calculator  ")
+print("For Adding '+' \nSubtract '-' \nMultiply '*' \nDivid '/' \nResult '=' \nPower '^' \nRoot 'sqrt'")
+S = None
 
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
+while True:
+    
+    j = input("\nEnter operator:")
+    
+    if j == "=":
+        print("Result is:",S)
+        break
+    elif j == "sin":
+        if S is None:
+            print("Enter a number first")
+        else:
+            rad = math.radians(S)
+            print("Converting to radians..........")
+            S = math.sin(rad)
+            
+        
+        
+    
+    try:
+        i = float(input("Enter a number: "))
+    except ValueError:
+        print("Invalid number ❌")
+        continue
+    
+    if S is None:
+        S = i
+        continue
+    
+    if j == "+":
+        S += i
+        
+    elif j == "-":
+        S -= i 
+    
+    elif j == "*":
+        S *= i
+        
+    elif j == "/":
+        if i == 0:
+            print("Divisible by zero is not define........")
+        else:   
+            S /= i
+    
+    elif j == "^":
+        n = float(input("Enter the power:"))
+        S = i ** n
+        
+    elif j == "sqrt":
+        if S is None:
+            S = float(input("Enter a number to be rooted:"))
+        elif S < 0:
+            print("Negetive cannot be rooted.......")
+        else:
+            S = math.sqrt(S)
+    elif j == "sin":
+        if S is None:
+            print("Enter a number first")
+        else:
+            rad = math.radians(S)
+            print("Converting to radians..........")
+            S = math.sin(rad)
+            
+    
+        
     else:
-        return 'Error: Division by zero'
-
-# Example usage
-if __name__ == '__main__':
-    print(add(1, 2))
-    print(subtract(5, 2))
-    print(multiply(3, 4))
-    print(divide(10, 2))
+        print("Invalid enter ..... ")
